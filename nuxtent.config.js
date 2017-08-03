ad3024a71415dede6143f525985d0f5ec3e32e95
Levi Bostian <levi.bostian@gmail.com>
2017-08-02 19:32:41 -0500

    Update nuxtent version to newest.

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -3,53 +3,118 @@ module.exports = {
     // routeName: 'freelance-slug', // pages/projects/_index.vue
     // permalink: 'freelance/:slug', // content/freelance_work/foo.md -> /freelance_work/foo
     // isPost: false
-    ['freelance', {
-      routeName: 'freelance-slug', // pages/projects/_index.vue
-      permalink: 'freelance/:slug', // content/freelance_work/foo.md -> /freelance_work/foo
-      isPost: false
+    ["freelance", {
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/freelance/_slug',
+          method: 'get'
+        }, {
+          path: '/freelance',
+          method: 'getAll'
+        }
+      ]
     }],
     ["internships", {
-      routeName: "internships-slug",
-      permalink: "internships/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/internships/_slug',
+          method: 'get'
+        }, {
+          path: '/internships',
+          method: 'getAll'
+        }
+      ]
     }],
     ["mentoring", {
-      routeName: "mentoring-slug",
-      permalink: "mentoring/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/mentoring/_slug',
+          method: 'get'
+        }, {
+          path: '/mentoring',
+          method: 'getAll'
+        }
+      ]
     }],
     ["opensource", {
-      routeName: "opensource-slug",
-      permalink: "opensource/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/opensource/_slug',
+          method: 'get'
+        }, {
+          path: '/opensource',
+          method: 'getAll'
+        }
+      ]
     }],
     ["products", {
-      routeName: "products-slug",
-      permalink: "products/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/products/_slug',
+          method: 'get'
+        }, {
+          path: '/products',
+          method: 'getAll'
+        }
+      ]
     }],
     ["services", {
-      routeName: "services-slug",
-      permalink: "services/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/services/_slug',
+          method: 'get'
+        }, {
+          path: '/services',
+          method: 'getAll'
+        }
+      ]
     }],
     ["talks", {
-      routeName: "talks-slug",
-      permalink: "talks/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/talks/_slug',
+          method: 'get'
+        }, {
+          path: '/talks',
+          method: 'getAll'
+        }
+      ]
     }],
     ["tutorials", {
-      routeName: "tutorials-slug",
-      permalink: "tutorials/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/tutorials/_slug',
+          method: 'get'
+        }, {
+          path: '/tutorials',
+          method: 'getAll'
+        }
+      ]
     }],
     ["writing", {
-      routeName: "writing-slug",
-      permalink: "writing/:slug",
-      isPost: false
+      permalink: ":slug",
+      isPost: false,
+      routes: [{
+          path: '/writing/_slug',
+          method: 'get'
+        }, {
+          path: '/writing',
+          method: 'getAll'
+        }
+      ]
     }]
   ],
   api: {
-    baseURL: (isProd) => isProd ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000'
-  }
+    baseURL: process.env.NODE_ENV === 'production'
+    ? 'http://127.0.0.1:3000'
+    : 'http://127.0.0.1:3000'
+ }
 }
